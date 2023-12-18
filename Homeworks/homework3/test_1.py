cacab18926d096ce1f0fdb03249e507abd3f53ca
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-12-18 17:35:48 +0300

    add homework3

---
+++ Homeworks/homework3/test_1.py
@@ -0,0 +1,90 @@
+from testpage import OperationsHelper
+import logging
+import yaml
+import time
+
+with open(r"C:\Users\Оля\PycharmProjects\pythonAPI2\Homeworks\homework3\testdata.yaml") as f:
+    testdata = yaml.safe_load(f)
+
+
+def test_step1(browser):
+    logging.info("Test1 starting")
+    testpage = OperationsHelper(browser, testdata['address'])
+    testpage.go_to_site()
+    testpage.enter_login("test")
+    testpage.enter_pass("test")
+    testpage.click_login_button()
+    assert testpage.get_error_text() == "401"
+
+
+def test_step2(browser):
+    logging.info("Test2 starting")
+    testpage = OperationsHelper(browser, testdata['address'])
+    testpage.go_to_site()
+    testpage.enter_login(testdata["login"])
+    testpage.enter_pass(testdata["password"])
+    testpage.click_login_button()
+    assert testpage.get_text_blog() == "Blog"
+
+
+def test_step3(browser):
+    # залогинится
+    logging.info("Test3 starting")
+    testpage = OperationsHelper(browser, testdata['address'])
+    testpage.go_to_site()
+    testpage.enter_login(testdata["login"])
+    testpage.enter_pass(testdata["password"])
+    testpage.click_login_button()
+
+    # создания поста
+    testpage.click_nwe_post_button()
+    testpage.enter_title_post("Homework3")
+    testpage.enter_content_post("The best Lesson")
+    testpage.click_create_nwe_post_button()
+
+    time.sleep(3)
+
+    # Проверка наличия названия поста на странице
+    assert testpage.post() == "Homework3"
+
+    # Дз--------------------------------------------------------------------------------------------
+
+
+def test_step4(browser):
+    logging.info("Test4 starting")
+    testpage = OperationsHelper(browser, testdata['address'])
+    testpage.go_to_site()
+    testpage.enter_login(testdata["login"])
+    testpage.enter_pass(testdata["password"])
+    testpage.click_login_button()
+    testpage.click_сontact_button()
+    time.sleep(2)
+    assert testpage.result_switcying_to_form_text() == "Contact us!"
+
+
+def test_step5(browser):
+    # залогинится
+    logging.info("Test5 starting")
+    testpage = OperationsHelper(browser, testdata['address'])
+    testpage.go_to_site()
+    testpage.enter_login(testdata["login"])
+    testpage.enter_pass(testdata["password"])
+    testpage.click_login_button()
+
+    # переход на форму
+    testpage.click_сontact_button()
+
+    # заполнение полей формы
+    testpage.enter_name("some name")
+    testpage.enter_email("some@email.ru")
+    testpage.enter_content_to_form("message")
+    testpage.click_contact_us_button()
+
+    time.sleep(3)
+
+    # Проверка надписи - Form successfully submitted (alert)
+    assert testpage.get_alert_text() == "Form successfully submitted"
+#
+
+
+
